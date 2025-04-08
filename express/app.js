75d758830f18c4d3dd1a7391b79617a4fdb60779
Noodle96 <jorgealfredomarino2@gmail.com>
2025-04-08 13:29:35 -0500

    Filtrar por uno o varios tipos,routers

--- express/app.js
+++ express/app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const {infoCartas}  = require('./cartas');
 
+// Routers: Reutilizar la parte inicial de un path
+const routerComunes = express.Router();
+app.use('/api/cartas/comunes', routerComunes);
+
 // console.log(infoCartas);
 // Routing
 app.get('/', (request, response) => {
@@ -15,9 +19,67 @@ app.get('/api/cartas', (request, response) => {
 app.get('/api/cartas/legendarias', (request, response) => {
     response.send(infoCartas.legendarias);
 });
-app.get('/api/cartas/comunes', (request, response) => {
+// app.get('/api/cartas/comunes', (request, response) => {
+routerComunes.get('/', (request, response) => {
     response.send(infoCartas.comunes);
 });
+
+app.get('/api/cartas/elixir/:costoElixir', (request, response) => {
+    let costoElixir = request.params.costoElixir;
+    console.log(request.params); // { tipo: 'especiales' }
+    console.log(typeof costoElixir); // string|
+    costoElixir = parseInt(costoElixir);
+    console.log(typeof costoElixir); // number
+
+    const comunesElixir3 = infoCartas.comunes.filter(carta => carta.costo === costoElixir);
+    const especialesElixir3 = infoCartas.especiales.filter(carta => carta.costo === costoElixir);
+    const epicasElixir3 = infoCartas.epicas.filter(carta => carta.costo === costoElixir);
+    const legendariasElixir3 = infoCartas.legendarias.filter(carta => carta.costo === costoElixir);
+    const resultadoFinal = [
+        ...comunesElixir3,
+        ...especialesElixir3,
+        ...epicasElixir3,
+        ...legendariasElixir3
+    ];
+    if(resultadoFinal.length === 0){
+        return response.status(404).send('No hay cartas con ese costo de elixir');
+    }
+    return response.send(JSON.stringify(resultadoFinal));
+});
+app.get('/api/cartas/:tipo', (request, response) => {
+    const tipo = request.params.tipo;
+    const resultado = infoCartas[tipo];
+    if(resultado.length === 0){
+        return response.status(404).send('No hay cartas de ese tipo');
+    }
+    return response.send(JSON.stringify(resultado));
+});
+
+// FILTRAR POR MAS DE UN PARAMETRO Y
+// PARAMETROS QUERY =>  Ordenarlos en base a su daño
+// app.get('/api/cartas/comunes/:tipo/:costo', (request, response)  => {
+routerComunes.get('/:tipo/:costo', (request, response)  => {
+    // console.log(request.params); // { tipo: 'especiales' }
+    // console.log(typeof request.params); // object
+    const tipo = request.params.tipo;
+    let costo = request.params.costo;
+    costo = parseInt(costo);
+    const resultado = infoCartas.comunes.filter(carta =>
+        carta.tipo === tipo && carta.costo === costo
+    );
+    if(resultado.length === 0){
+        return response.status(404).send('No hay cartas de ese tipo y costo');
+    }
+    console.log("==> request.query", request.query);
+    console.log(request.query.ordenar); // undefined si no existe ordenar
+
+    // http://localhost:3000/api/cartas/comunes/tierra/3?ordenar=vistas
+    if(request.query.ordenar === 'danhio'){ // si no hay seria undefined
+        resultado.sort((a, b) => a.daño - b.daño);
+    }
+    return response.status(200).send(JSON.stringify(resultado))
+});
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
--- express/cartas.js
+++ express/cartas.js
@@ -3,7 +3,7 @@ let infoCartas = {
     'comunes': [
         {
             'nombre': 'Caballero',
-            'tipo': 'Tierra',
+            'tipo': 'tierra',
             'costo': 3,
             'daño': 100,
             'vida': 800,
@@ -12,18 +12,28 @@ let infoCartas = {
         },
         {
             'nombre': 'Esbirros',
-            'tipo': 'Aéreo',
+            'tipo': 'aereo',
             'costo': 3,
             'daño': 50,
             'vida': 200,
             'velocidad': 1.5,
             'alcance': 2
         },
+        {
+            'nombre': 'Esqueleto',
+            'tipo': 'tierra',
+            'costo': 3,
+            'daño': 30,
+            'vida': 100,
+            'velocidad': 1.5,
+            'alcance': 1
+        },
+
     ],
     'especiales': [
         {
             'nombre': 'Mago',
-            'tipo': 'Tierra',
+            'tipo': 'tierra',
             'costo': 5,
             'daño': 150,
             'vida': 600,
@@ -31,19 +41,59 @@ let infoCartas = {
             'alcance': 5
         },
         {
-            'nombre': 'Dragón',
-            'tipo': 'Aéreo',
+            'nombre': 'Montapuercos',
+            'tipo': 'tierra',
+            'costo': 4,
+            'daño': 200,
+            'vida': 600,
+            'velocidad': 1.8,
+            'alcance': 1
+        },
+        {
+            'nombre': 'Lapida',
+            'tipo': 'tierra',
+            'costo': 3,
+            'daño': 0,
+            'vida': 800,
+            'velocidad': 0,
+            'alcance': 4
+        },
+
+    ],
+    'epicas': [
+        {
+            'nombre': 'Golem',
+            'tipo': 'tierra',
+            'costo': 8,
+            'daño': 300,
+            'vida': 3000,
+            'velocidad': 0.5,
+            'alcance': 1
+        },
+        {
+            'nombre': 'Torre Tesla',
+            'tipo': 'tierra',
+            'costo': 3,
+            'daño': 1000,
+            'vida': 900,
+            'velocidad': 1.2,
+            'alcance': 5
+        },
+        {
+            'nombre': 'Baby Dragón',
+            'tipo': 'aereo',
             'costo': 4,
             'daño': 120,
             'vida': 500,
             'velocidad': 1.5,
             'alcance': 3
         },
+
     ],
     'legendarias': [
         {
             'nombre': 'Princesa',
-            'tipo': 'Tierra',
+            'tipo': 'tierra',
             'costo': 3,
             'daño': 200,
             'vida': 400,
@@ -52,7 +102,7 @@ let infoCartas = {
         },
         {
             'nombre': 'Chispitas',
-            'tipo': 'Tierra',
+            'tipo': 'tierra',
             'costo': 6,
             'daño': 300,
             'vida': 800,
